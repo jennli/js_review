a68130995c46660814d14198c8a61b5664444717
Jennifer Li <ldnjennifer@hotmail.com>
2016-01-23 15:22:03 -0800

    refactoring ghost.js

--- ghost.js
+++ ghost.js
@@ -1,20 +1,3 @@
 var Ghost = function(){
-  var randy = Math.floor(Math.random()*4 + 1);
-  console.log (randy);
-  switch (randy){
-    case 1:
-    this.color = "red";
-    break;
-
-    case 2:
-    this.color = "pink";
-    break;
-
-    case 3:
-    this.color = "white";
-    break;
-
-    default:
-    this.color = "yellow";
-  }
+this.color = ["red", "yellow", "pink", "white"][Math.floor(Math.random()*4)];
 }
